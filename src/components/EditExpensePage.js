702174ef8b7d75df52d1be3c4615c7fd6d0fec3b
Govind <govind.g@turtlemint.com>
2017-12-09 23:16:51 +0530

    Division into component.

---
+++ src/components/EditExpensePage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const EditExpensePage = () => (
+  <div>
+    This is edit page component
+  </div>
+);
+
+export default EditExpensePage;
--- src/components/component1.js
+++
@@ -1,3 +0,0 @@
-/**
- * Created by home on 07/12/17.
- */
